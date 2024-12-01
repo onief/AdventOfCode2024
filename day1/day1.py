ab430a11eb75836e98a10a7d5a629c279d530b57
onief <o.nief@t-online.de>
2024-12-01 22:54:33 +0100

    Solve Day1

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,17 @@
 import sys
+from collections import Counter
 
 inputs = [line.split() for line in sys.stdin]
-print(inputs)
+
+# 1)
+left = sorted([int(x[0]) for x in inputs])
+right = sorted([int(x[1]) for x in inputs])
+
+pairs = zip(left, right)
+result1 = sum([abs(x - y) for (x, y) in pairs])
+print(result1)
+
+# 2)
+right_counts = Counter(right)
+result2 = sum([x * right_counts[x] for x in left])
+print(result2)
